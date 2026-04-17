894899e3df0bd3674de0911e2daeafd847bd289b
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2026-04-17 18:53:09 +0100

    🔧 Update build configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'go/dist',
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       '/plane-api': {
